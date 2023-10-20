14a88aee378ad4c095c9ec0a9a38abd9633328e5
piaolivera <piaoliveraa@gmail.com>
2023-10-20 12:59:41 -0300

    tienda libros

--- Leccion03/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Leccion03/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -5,17 +5,30 @@ import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @Component
 public class LibroFrom extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private JTextField LibroTexto;
+    private JLabel Autor;
+    private JTextField autorTexto;
+    private JPanel precio;
+    private JButton agregarButton;
+    private DefaultTableModel tablaModeloLibros;
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio){
         this.libroServicio = libroServicio;
         iniciarForma();
+        agregarButton.addActionListener(e -> {
+
+        });
     }
 
     private void iniciarForma(){
@@ -30,5 +43,33 @@ public class LibroFrom extends JFrame {
         int y = (tamanioPantalla.height - getHeight()/2);
         setLocation(x, y);
 
+    }
+
+    private void createUIComponents() {
+        this.tablaModeloLibros = new DefaultTableModel(0, 5);
+        String[] cabecera = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+        // Instanciar el objeto de JTable
+        this.tablaLibros = new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        //limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+        //Obtener los libros de la BD
+        var libros = libroServicio.listarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro) ->{ //Función lambda
+            //Creamos cada registro para agregarlos a la tabla
+            Object [] renglonLibro ={
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
     }
 }
